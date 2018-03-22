09ce8ec298a81df8bc85f567f00c06a60d8a2f35
vv0rkman <vv0rkman@gmail.com>
2018-03-22 21:46:16 +0200

    created entity

--- src/com/vv0rkman/entity/Project.java
+++ src/com/vv0rkman/entity/Project.java
@@ -1,50 +1,95 @@
 package com.vv0rkman.entity;
 
-public class Project {
+import com.vv0rkman.service.Tools;
 
-    private int id;
-    private int id_cust;
-    private String name = "";
-    private String start_Date = "";
-    private String end_Date = "";
+import java.sql.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
-    public int getId() {
-        return id;
+public class Project extends Entity {
+
+    private long id_cust;
+    private Date start_Date;
+    private Date end_Date;
+
+    public Project() {
+
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public Project(String name, long id_cust, Date start_Date, Date end_Date) {
+
+        super(Tools.generateID(), id_cust, name);
+
+        this.id_cust = id_cust;
+
+        this.start_Date = start_Date;
+
+        this.end_Date = end_Date;
+
     }
 
-    public int getId_cust() {
+    public Project(long id, String name, long id_cust, Date start_Date, Date end_Date) {
+
+        super(id, id_cust, name);
+
+        this.id_cust = id_cust;
+
+        this.start_Date = start_Date;
+
+        this.end_Date = end_Date;
+    }
+
+    public long getId_cust() {
         return id_cust;
     }
 
-    public void setId_cust(int id_cust) {
+    public void setId_cust(long id_cust) {
         this.id_cust = id_cust;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getStart_Date() {
+    public Date getStart_Date() {
         return start_Date;
     }
 
-    public void setStart_Date(String start_Date) {
+    public void setStart_Date(Date start_Date) {
         this.start_Date = start_Date;
     }
 
-    public String getEnd_Date() {
+    public Date getEnd_Date() {
         return end_Date;
     }
 
-    public void setEnd_Date(String end_Date) {
+    public void setEnd_Date(Date end_Date) {
         this.end_Date = end_Date;
+    }
+
+    /**
+     * @param params
+     * key == attr_id
+     * value == params.value
+     */
+    public void setData(LinkedHashMap params) {
+
+        for (Object aParamsSet : params.entrySet()) {
+
+            Map.Entry paramEntry = (Map.Entry) aParamsSet;
+
+            if (paramEntry.getKey().equals(NAME)) {
+
+                setName(paramEntry.getValue().toString());
+
+            } else if (paramEntry.getKey().equals(PARENT_ID)) {
+
+                setId_cust((Long) paramEntry.getValue());
+
+            } else if (paramEntry.getKey().equals(3)) {
+
+                setStart_Date((Date) paramEntry.getValue());
+
+            } else if (paramEntry.getKey().equals(4)) {
+
+                setEnd_Date((Date) paramEntry.getValue());
+            }
+        }
     }
 }
